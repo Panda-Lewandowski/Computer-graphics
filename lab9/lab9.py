8923cc455ac10f04d2d2581a3ddea02e411594d1
Pandas <daria-shcherbatyuk@yandex.ru>
2017-05-25 14:27:32 +0300

    fix is_intersection ans visiable

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -9,6 +9,7 @@ blue = Qt.blue
 black = Qt.black
 now = None
 
+
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
@@ -16,21 +17,20 @@ class Window(QtWidgets.QMainWindow):
         self.scene = Scene(0, 0, 561, 581)
         self.scene.win = self
         self.view.setScene(self.scene)
-        self.pol.clicked.connect(lambda : set_pol(self))
+        self.poly.clicked.connect(lambda : set_pol(self))
         self.erase.clicked.connect(lambda: clean_all(self))
         self.paint.clicked.connect(lambda: clipping(self))
         self.rect.clicked.connect(lambda: set_rect(self))
         self.ect.clicked.connect(lambda: add_bars(self))
         self.lock.clicked.connect(lambda: lock(self))
-        self.pol_clip = []
-        self.pol_pol = []
-        self.clip = None
-        self.point_now_rect = None
+        self.clip = []
+        self.pol = []
+        self.point_now_clip = None
         self.point_now_pol = None
         self.point_lock_pol = None
-        self.point_lock_rect = None
+        self.point_lock_clip = None
         self.input_pol = False
-        self.input_rect = False
+        self.input_clip = False
         self.pen = QPen(black)
 
 
@@ -69,15 +69,15 @@ def set_pol(win):
 
 
 def set_rect(win):
-    if win.input_rect:
-        win.input_rect = False
-        win.pol.setDisabled(False)
+    if win.input_clip:
+        win.input_clip = False
+        win.poly.setDisabled(False)
         win.erase.setDisabled(False)
         win.paint.setDisabled(False)
         win.ect.setDisabled(False)
     else:
-        win.input_rect = True
-        win.pol.setDisabled(True)
+        win.input_clip = True
+        win.poly.setDisabled(True)
         win.erase.setDisabled(True)
         win.paint.setDisabled(True)
         win.ect.setDisabled(True)
@@ -85,11 +85,11 @@ def set_rect(win):
 
 def add_point(point):
     global w
-    if w.input_rect:
+    if w.input_clip:
         w.pen.setColor(black)
-        if w.point_now_rect is None:
-            w.point_now_rect = point
-            w.point_lock_rect = point
+        if w.point_now_clip is None:
+            w.point_now_clip = point
+            w.point_lock_clip = point
             add_row(w.table_rect)
             i = w.table_rect.rowCount() - 1
             item_x = QTableWidgetItem("{0}".format(point.x()))
@@ -97,8 +97,8 @@ def add_point(point):
             w.table_rect.setItem(i, 0, item_x)
             w.table_rect.setItem(i, 1, item_y)
         else:
-            w.pol_clip.append(point)
-            w.point_now_rect = point
+            w.clip.append(point)
+            w.point_now_clip = point
             add_row(w.table_rect)
             i = w.table_rect.rowCount() - 1
             item_x = QTableWidgetItem("{0}".format(point.x()))
@@ -121,7 +121,7 @@ def add_point(point):
             w.table_pol.setItem(i, 0, item_x)
             w.table_pol.setItem(i, 1, item_y)
         else:
-            w.pol_pol.append(point)
+            w.pol.append(point)
             w.point_now_pol = point
             add_row(w.table_pol)
             i = w.table_pol.rowCount() - 1
@@ -136,14 +136,14 @@ def add_point(point):
 
 def lock(win):
     if w.input_pol:
-        win.pol_clip.append(win.point_lock_pol)
+        win.pol.append(win.point_lock_pol)
         win.scene.addLine(win.point_now_pol.x(), win.point_now_pol.y(), win.point_lock_pol.x(), win.point_lock_pol.y(), w.pen)
         win.point_now_pol = None
 
-    if w.input_rect:
-        win.pol_pol.append(win.point_lock_rect)
-        win.scene.addLine(win.point_now_rect.x(), win.point_now_rect.y(), win.point_lock_rect.x(), win.point_lock_rect.y(), w.pen)
-        win.point_now_rect = None
+    if w.input_clip:
+        win.clip.append(win.point_lock_clip)
+        win.scene.addLine(win.point_now_clip.x(), win.point_now_clip.y(), win.point_lock_clip.x(), win.point_lock_clip.y(), w.pen)
+        win.point_now_clip = None
 
 
 def add_row(win_table):
@@ -154,11 +154,11 @@ def clean_all(win):
     win.scene.clear()
     win.table_rect.clear()
     win.table_pol.clear()
-    win.pol_clip = []
-    win.pol_pol = []
-    win.point_now_rect = None
+    win.clip = []
+    win.pol = []
+    win.point_now_clip = None
     win.point_now_pol = None
-    win.point_lock_rect = None
+    win.point_lock_clip = None
     win.point_lock_pol = None
     r = win.table_rect.rowCount()
     for i in range(r, -1, -1):
@@ -230,110 +230,43 @@ def isConvex(edges):
     return flag * curr
 
 
-def clipping(win):
-    if len(win.pol_clip) <= 1:
-        QMessageBox.warning(win, "Ошибка!", "Отсекатель не задан!")
-
-    if len(win.pol_pol) <= 1:
-        QMessageBox.warning(win, "Ошибка!", "Многоугольник не задан!")
-
-    if len(win.pol_pol) > 1 and len(win.pol_clip) > 1:
-        norm = isConvex(win.pol_clip)
-        if not norm:
-            QMessageBox.warning(win, "Ошибка!", "Отсекатель не выпуклый!Операция не может быть проведена!")
-        else:
-            p = sutherland_hodgman(win.pol_clip, win.pol_pol, norm)
-            if p:
-                win.pen.setWidth(2)
-                win.pen.setColor(red)
-                win.scene.addPolygon(p, win.pen)
-                win.pen.setWidth(1)
-
-
-def sutherland_hodgman(clip, pol, norm):
-    # дублируем начальную вершину отсекателя в конец
-    clip.append(clip[0])
-
-    s = None
-    f = None
-    for i in range(len(clip) - 1):
-        new = []
-        for j in range(len(pol)):
-            if j == 0:
-                f = pol[j]
-            else:
-                t = is_intersection([s, pol[j]], [clip[i], clip[i + 1]])
-                if t:
-                    new.append(t)
-
-            s = pol[j]
-            if is_visiable(s,  clip[i], clip[i + 1]):
-                    new.append(s)
-
-        if len(new) != 0:
-            t = is_intersection([s, f], [clip[i], clip[i + 1]])
-            if t:
-                new.append(t)
-            print(new)
-            new.append(new[0])
-            pol = copy.deepcopy(new)
-
-    if len(pol) == 0:
-        return False
-    else:
-        return QPolygonF(pol)
-
-
-def is_intersection(ed1, ed2):
-    if (is_visiable(ed1[0], ed2[0], ed2[1]) and not is_visiable(ed1[1], ed2[0], ed2[1])) or \
-            (not is_visiable(ed1[0], ed2[0], ed2[1]) and is_visiable(ed1[1], ed2[0], ed2[1])):
+def is_intersection(ed1, ed2, norm):
+    vis1 = is_visiable(ed1[0], ed2[0], ed2[1], norm)
+    vis2 = is_visiable(ed1[1], ed2[0], ed2[1], norm)
+    if (vis1 and not vis2) or (not vis1 and vis2):
         # ищем пересечение
-        a = ed1[0].x()
-        b = ed1[1].x() - ed1[0].x()
-        c = ed2[0].x()
-        d = ed2[1].x() - ed2[0].x()
 
-        e = ed1[0].y()
-        f = ed1[1].y() - ed1[0].y()
-        p = ed2[0].y()
-        m = ed2[1].y() - ed2[0].y()
+        p1 = ed1[0]
+        p2 = ed1[1]
 
-        t = (d * (e + p) - m * (a + c))/(d * f - b * m)
-        #print(t)
+        q1 = ed2[0]
+        q2 = ed2[1]
+
+        delta = (p2.x() - p1.x()) * (q1.y() - q2.y()) - (q1.x() - q2.x()) * (p2.y() - p1.y())
+        delta_t = (q1.x() - p1.x()) * (q1.y() - q2.y()) - (q1.x() - q2.x()) * (q1.y() - p1.y())
+
+        if abs(delta) <= 1e-6:
+            return p2
+
+        t = delta_t / delta
 
         if 0 <= t <= 1:
+
             I = QPointF()
             I.setX(ed1[0].x() + (ed1[1].x() - ed1[0].x()) * t)
             I.setY(ed1[0].y() + (ed1[1].y() - ed1[0].y()) * t)
-            #print(I.x(), I.y())
+
             return I
         else:
             return False
 
+
     else:
         return False
 
 
-def is_visiable(point, peak1, peak2):
-    """if line(peak1, peak2, point) * line(peak1, peak2, peak3) < 0:
-            return False
-        else:
-            return True"""
-
-    """W = QPointF()
-    W.setX(point.x() - peak1.x())
-    W.setY(point.y() - peak1.y())
-
-    N = QPointF()
-    N.setX(-n * (peak2.y() - peak1.y()))
-    N.setY(n * (peak2.x() - peak1.x()))
-
-    Wscalar = scalar(W, N)
-    if Wscalar >= 0:
-        return True
-    else:
-        return False"""
-    if vector([peak1, point], [peak1, peak2]) >= 0:
+def is_visiable(point, peak1, peak2, norm):
+    if norm * vector([peak1, point], [peak1, peak2]) <= 0:
         return True
     else:
         return False
@@ -346,21 +279,63 @@ def vector(v1, v2):
     x2 = v2[0].x() - v2[1].x()
     y2 = v2[0].y() - v1[1].y()
 
-    r = x1 * y2 - x2 * y1
-    return sign(r)
+    return x1 * y2 - x2 * y1
 
 
 
+def clipping(win):
+    if len(win.clip) <= 1:
+        QMessageBox.warning(win, "Ошибка!", "Отсекатель не задан!")
 
-def scalar(v1, v2):
-    return v1.x() * v2.x() + v1.y() * v2.y()
+    if len(win.pol) <= 1:
+        QMessageBox.warning(win, "Ошибка!", "Многоугольник не задан!")
 
+    if len(win.pol) > 1 and len(win.clip) > 1:
+        norm = isConvex(win.clip)
+        if not norm:
+            QMessageBox.warning(win, "Ошибка!", "Отсекатель не выпуклый!Операция не может быть проведена!")
+        else:
+            p = sutherland_hodgman(win.clip, win.pol, norm)
+            if p:
+                win.pen.setWidth(2)
+                win.pen.setColor(red)
+                win.scene.addPolygon(p, win.pen)
+                win.pen.setWidth(1)
 
-def line(p1, p2, p):
-    if (p1.y() - p2.y()) * p.x() + (p2.x() - p1.x()) * p.y() + (p1.x() * p2.y() - p2.x() * p1.y()) >= 0:
-        return 1
+# correct
+def sutherland_hodgman(clip, pol, norm):
+    # дублируем начальную вершину отсекателя в конец
+    clip.append(clip[0])
+
+    s = None
+    f = None
+    # цикл по вершинам отсекателя
+    for i in range(len(clip) - 1):
+        new = []  # новый массив вершин
+        for j in range(len(pol)):    # цикл по вершинам многоугольника
+            if j == 0:
+                f = pol[j]
+            else:
+                t = is_intersection([s, pol[j]], [clip[i], clip[i + 1]], norm)
+                if t:
+                    new.append(t)
+
+            s = pol[j]
+            if is_visiable(s,  clip[i], clip[i + 1], norm):
+                    new.append(s)
+
+        if len(new) != 0:
+            t = is_intersection([s, f], [clip[i], clip[i + 1]], norm)
+            if t:
+                new.append(t)
+
+            pol = copy.deepcopy(new)
+            print(new)
+
+    if len(pol) == 0:
+        return False
     else:
-        return -1
+        return QPolygonF(pol)
 
 
 if __name__ == "__main__":
