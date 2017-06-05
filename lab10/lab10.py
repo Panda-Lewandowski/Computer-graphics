cdad319f939b1ef3f16c53d66abe2b2984ca382e
Pandas <daria-shcherbatyuk@yandex.ru>
2017-06-05 16:13:13 +0300

    add functions

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -2,7 +2,7 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QGraphicsScene
 from PyQt5.QtGui import QPen, QImage, QPixmap
 from PyQt5.QtCore import Qt
-from math import sin, cos
+from math import sin, cos, exp, sqrt
 from horizon import float_horizon
 
 
@@ -26,10 +26,31 @@ class Window(QtWidgets.QMainWindow):
         self.dial_x.valueChanged.connect(lambda: draw(self))
         self.dial_y.valueChanged.connect(lambda: draw(self))
         self.dial_z.valueChanged.connect(lambda: draw(self))
+        self.funcs.addItem("cos(x) * sin(z)")
+        self.funcs.addItem("2 * cos(x * z)")
+        self.funcs.addItem("exp(sin(sqrt(x^2 + z^2)))")
+        self.funcs.addItem("x^2 / 20 + z^2 / 20")
+        self.funcs.addItem("|sin(x) * sin(z)|")
 
 
-def f(x, z):
+def f1(x, z):
     return cos(x) * sin(z)
+
+
+def f2(x, z):
+    return 2 * cos(x * z)
+
+
+def f3(x, z):
+    return exp(sin(sqrt(x**2 + z**2)))
+
+
+def f4(x, z):
+    return x**2 / 20 + z**2 / 20
+
+
+def f5(x, z):
+    return abs(sin(x) * sin(z))
 
 
 def draw(win):
@@ -38,6 +59,21 @@ def draw(win):
     tx = win.dial_x.value()
     ty = win.dial_y.value()
     tz = win.dial_z.value()
+
+    if win.funcs.currentText() == "cos(x) * sin(z)":
+        f = f1
+
+    if win.funcs.currentText() == "2 * cos(x * z)":
+        f = f2
+
+    if win.funcs.currentText() == "exp(sin(sqrt(x^2 + z^2)))":
+        f = f3
+
+    if win.funcs.currentText() == "x^2 / 20 + z^2 / 20":
+        f = f4
+
+    if win.funcs.currentText() == "|sin(x) * sin(z)|":
+        f = f5
 
     win.image = float_horizon(win.scene.width(), win.scene.height(), win.x_min.value(), win.x_max.value(), win.dx.value(),
             win.z_min.value(), win.z_max.value(), win.dz.value(), tx, ty, tz, f, win.image)
